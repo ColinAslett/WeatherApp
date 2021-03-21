e22021a22ea2221aa27cc3cfb8de8540b4f97d2e
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-03-21 10:12:25 -0700

    Added NBP, this can probably be condensed will put on to do list

--- Downloader.java
+++ Downloader.java
@@ -6,9 +6,9 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class Downloader {
-	//NBE ARRAYLIST
+	//NBM ARRAYLISTs
 	ArrayList<String> NBE_RAW = new ArrayList<>();
-	ArrayList<String[]> NBE_TRANSFORMED = new ArrayList<>();
+	ArrayList<String> NBP_RAW = new ArrayList<>();
 	//Data Class
 	Data data;
 	public Downloader(Data d){
@@ -38,5 +38,29 @@ public class Downloader {
 			
 		}
 		data.NBE(NBE_RAW);
+	}
+	public void NOAA_NBP(String LINK, String station) throws Exception{
+		System.out.println(LINK);
+		URL url = new URL(LINK);
+		InputStream is = url.openStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+		int count = 0;
+		boolean atStation = false;
+		while((line = br.readLine()) != null){
+			if(atStation == true){
+				count++;
+				NBP_RAW.add(line);
+				if(line.contains("I24P9")){
+					break;
+				}
+			}
+			if(line.contains(station)){
+				//Continue until we get to the SOL line
+				atStation = true;
+			}
+			
+		}
+		data.NBP(NBP_RAW);
 	}
 }
