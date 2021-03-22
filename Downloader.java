05c9a9e04847755bf7a548dfc9d0f46179cbe842
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-03-22 14:42:41 -0700

    Now Downloads HRRR and NAM Data

--- Downloader.java
+++ Downloader.java
@@ -9,6 +9,10 @@ public class Downloader {
 	//NBM ARRAYLISTs
 	ArrayList<String> NBE_RAW = new ArrayList<>();
 	ArrayList<String> NBP_RAW = new ArrayList<>();
+	//NAM
+	ArrayList<String> NAM_RAW = new ArrayList<>();
+	//HRRR
+	ArrayList<String> HRRR_RAW = new ArrayList<>();	
 	//Data Class
 	Data data;
 	public Downloader(Data d){
@@ -27,12 +31,12 @@ public class Downloader {
 			if(atStation == true){
 				count++;
 				NBE_RAW.add(line);
+				//Continue until we get to the SOL line
 				if(line.contains("SOL")){
 					break;
 				}
 			}
 			if(line.contains(station)){
-				//Continue until we get to the SOL line
 				atStation = true;
 			}
 			
@@ -51,16 +55,64 @@ public class Downloader {
 			if(atStation == true){
 				count++;
 				NBP_RAW.add(line);
+				//Continue until we get to the I2499 line
 				if(line.contains("I24P9")){
 					break;
 				}
 			}
 			if(line.contains(station)){
-				//Continue until we get to the SOL line
 				atStation = true;
 			}
 			
 		}
 		data.NBP(NBP_RAW);
+	}
+	public void NAM_THUNDER(String LINK) throws Exception{
+		System.out.println(LINK);
+		URL url = new URL(LINK);
+		InputStream is = url.openStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+		while((line = br.readLine()) != null){
+			if(line.contains("YYMMDD/HHMM")){//This is the other part of NAM
+				break;
+			}
+			else{
+				NAM_RAW.add(line);
+			}
+		}
+		ArrayList<String> temp_NAM = new ArrayList<>();
+		//Removing unnecessary atmosphere pressure lines
+		for(int i = 0;i < NAM_RAW.size();i++){
+			if(NAM_RAW.get(i).contains("STIM") || NAM_RAW.get(i).contains("SHOW") 
+					|| NAM_RAW.get(i).contains("LCLP") || NAM_RAW.get(i).contains("LCLT") || NAM_RAW.get(i).contains("BRCH")){
+				temp_NAM.add(NAM_RAW.get(i));
+			}
+		}
+		data.NAM(temp_NAM);
+	}
+	public void HRRR_THUNDER(String LINK) throws Exception{
+		System.out.println(LINK);
+		URL url = new URL(LINK);
+		InputStream is = url.openStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+		while((line = br.readLine()) != null){
+			if(line.contains("YYMMDD/HHMM")){//This is the other part of NAM
+				break;
+			}
+			else{
+				HRRR_RAW.add(line);
+			}
+		}
+		ArrayList<String> temp_NAM = new ArrayList<>();
+		//Removing unnecessary atmosphere pressure lines
+		for(int i = 0;i < HRRR_RAW.size();i++){
+			if(HRRR_RAW.get(i).contains("STIM") || HRRR_RAW.get(i).contains("SHOW") 
+					|| HRRR_RAW.get(i).contains("LCLP") || HRRR_RAW.get(i).contains("LCLT") || HRRR_RAW.get(i).contains("BRCH")){
+				temp_NAM.add(HRRR_RAW.get(i));
+			}
+		}
+		data.HRRR(temp_NAM);		
 	}
 }
